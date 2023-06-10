c73e904d39c0d480ad01414969732f9d4d45222b
iSenjougahara <gaahchimente@hotmail.com>
2023-06-10 18:00:25 -0300

    commit 3?

--- src/java/model/ModeloCarro.java
+++ src/java/model/ModeloCarro.java
@@ -1,13 +1,111 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Gabriel
- */
+import java.util.Date;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.sql.*;
+import java.util.ArrayList;
+import web.AppListener;
+
 public class ModeloCarro {
+
+    private long rowId;
+    private String nomeModelo;
+    private Date ano;
+    private String marca;
+
+    public ModeloCarro(long rowId, String nomeModelo, Date ano, String marca) {
+        this.rowId = rowId;
+        this.nomeModelo = nomeModelo;
+        this.ano = ano;
+        this.marca = marca;
+    }
+
+    // Getters and setters for the attributes
+    public static String getCreateStatement() {
+        return "CREATE TABLE IF NOT EXISTS modeloCarro("
+                + "rowId BIGINT PRIMARY KEY,"
+                + "nomeModelo VARCHAR(255),"
+                + "ano DATE,"
+                + "marca VARCHAR(255)"
+                + ")";
+    }
     
+     public static void insertModeloCarro(String nomeModelo, Date ano, String marca) {
+        try {
+            Connection connection = AppListener.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO modeloCarro (nomeModelo, ano, marca) VALUES (?, ?, ?)"
+            );
+            statement.setString(1, nomeModelo);
+            statement.setDate(2, new java.sql.Date(ano.getTime()));
+            statement.setString(3, marca);
+            statement.executeUpdate();
+            statement.close();
+            connection.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            // Handle any exceptions
+        }
+    }
+     
+    
+
+   public static ArrayList<ModeloCarro> getModeloCarros() throws Exception {
+    ArrayList<ModeloCarro> list = new ArrayList<>();
+    Connection con = AppListener.getConnection();
+    Statement stmt = con.createStatement();
+    ResultSet rs = stmt.executeQuery("SELECT rowid, * from modeloCarro");
+    while (rs.next()) {
+        long rowId = rs.getLong("rowid");
+        String nomeModelo = rs.getString("nomeModelo");
+        Date ano = rs.getDate("ano");
+        String marca = rs.getString("marca");
+        ModeloCarro modeloCarro = new ModeloCarro(rowId, nomeModelo, ano, marca);
+        list.add(modeloCarro);
+    }
+    rs.close();
+    stmt.close();
+    con.close();
+    return list;
+}
+
+
+    // ...
+
+
+
+
+    public long getRowId() {
+        return rowId;
+    }
+
+    public void setRowId(long rowId) {
+        this.rowId = rowId;
+    }
+
+    public String getNomeModelo() {
+        return nomeModelo;
+    }
+
+    public void setNomeModelo(String nomeModelo) {
+        this.nomeModelo = nomeModelo;
+    }
+
+    public Date getAno() {
+        return ano;
+    }
+
+    public void setAno(Date ano) {
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 }
--- src/java/web/AppListener.java
+++ src/java/web/AppListener.java
@@ -1,13 +1,104 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package web;
 
-/**
- *
- * @author Gabriel
- */
-public class AppListener {
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import java.util.Date;
+import java.sql.*;
+import java.security.*;
+import java.math.*;
+import model.ModeloCarro;
+import model.Pecas;
+
+@WebListener
+public class AppListener implements ServletContextListener{
+public static final String CLASS_NAME = "org.sqlite.JDBC";
+    public static final String URL = "jdbc:sqlite:parkapp.db";
+    public static String initializeLog = "";
+    public static Exception exception = null;
     
+    
+    
+    
+     public static Connection getConnection() throws Exception {
+        Class.forName(CLASS_NAME);
+        return DriverManager.getConnection(URL);
+    }
+ public static String getMd5Hash(String text) throws NoSuchAlgorithmException {
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(text.getBytes(), 0, text.length());
+        return new BigInteger(1, m.digest()).toString();
+    }
+    @Override
+   public void contextInitialized(ServletContextEvent sce) {
+    try {
+        Connection c = AppListener.getConnection();
+        Statement s = c.createStatement();
+        initializeLog += new Date() + ": Initializing database creation; ";
+        
+        // Pecas
+        initializeLog += "Creating Pecas table...";
+        s.execute(Pecas.getCreateStatement());
+        initializeLog += "done; ";
+        
+        // ModeloCarro
+        initializeLog += "Creating ModeloCarro table...";
+        s.execute(ModeloCarro.getCreateStatement());
+        initializeLog += "done; ";
+        if (Pecas.getPecas().isEmpty()) {
+    initializeLog += "Adding default Pecas...";
+    Pecas.insertPecas("Peca 1", 10.0, 1);
+    Pecas.insertPecas("Peca 2", 20.0, 2);
+    Pecas.insertPecas("Peca 3", 30.0, 3);
+    initializeLog += "Pecas added; ";
 }
+
+if (ModeloCarro.getModeloCarros().isEmpty()) {
+    initializeLog += "Adding default ModeloCarros...";
+    ModeloCarro.insertModeloCarro("ModeloCarro 1", new Date(), "Marca 1");
+    ModeloCarro.insertModeloCarro("ModeloCarro 2", new Date(), "Marca 2");
+    ModeloCarro.insertModeloCarro("ModeloCarro 3", new Date(), "Marca 3");
+    initializeLog += "ModeloCarros added; ";
+}
+
+     initializeLog += "Fetching recently inserted data from Pecas table...\n";
+
+try {
+    Connection con = AppListener.getConnection();
+    Statement stmt = con.createStatement();
+    ResultSet rs = stmt.executeQuery("SELECT * FROM Pecas");
+
+    initializeLog += "Recently inserted data:\n";
+
+    while (rs.next()) {
+        String nomePeca = rs.getString("nomePeca");
+        double preco = rs.getDouble("preco");
+        long modeloCarroRowId = rs.getLong("modeloCarro_rowId");
+
+        initializeLog += "Nome da Peça: " + nomePeca + ", Preço: " + preco + ", Modelo Carro RowId: " + modeloCarroRowId + "\n";
+    }
+
+    rs.close();
+    stmt.close();
+    con.close();
+} catch (Exception ex) {
+    initializeLog += "Error fetching data: " + ex.getMessage();
+}
+
+initializeLog += "Data fetch completed.\n";
+        
+        s.close();
+        c.close();
+    } catch (Exception ex) {
+        initializeLog += "Error: " + ex.getMessage();
+    }
+}
+
+    }
+    
+    
+     
+    
+    
+
